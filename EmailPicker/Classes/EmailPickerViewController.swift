a53fcb95eb08a860728193afcc589a057adddc65
Christian Hatch <christianhatch@gmail.com>
2019-08-21 12:45:37 -0400

    Helper to run code in the background

--- EmailPicker/Classes/EmailPickerViewController.swift
+++ EmailPicker/Classes/EmailPickerViewController.swift
@@ -531,13 +531,15 @@ private extension String {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
+private extension DispatchQueue {
+    static func background(delay: Double = 0.0, background: (()->Void)? = nil, completion: (() -> Void)? = nil) {
+        DispatchQueue.global(qos: .background).async {
+            background?()
+            if let completion = completion {
+                DispatchQueue.main.asyncAfter(deadline: .now() + delay, execute: {
+                    completion()
+                })
+            }
+        }
+    }
+}
